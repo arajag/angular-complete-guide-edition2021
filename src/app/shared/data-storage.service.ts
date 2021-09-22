f1a20ebbe64131d092a72d225ce69406807160ca
acrajag <andre.raja@gmail.com>
2021-09-22 21:35:14 +0100

    19.281 - Fetching Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
+import {Recipe} from '../recipes/recipe.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,10 @@ export class DataStorageService {
       .subscribe(
         response => console.log(response)
       );
+  }
+
+  fetchRecipes(){
+    this.http.get<Recipe[]>('https://ng-course-recipe-book-74da8-default-rtdb.firebaseio.com/recipes.json')
+      .subscribe(recipes => this.recipeService.setRecipes(recipes));
   }
 }
