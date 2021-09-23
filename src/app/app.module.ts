8d32de6a95c2ec695d19488cf342995e1d0bff04
acrajag <andre.raja@gmail.com>
2021-09-23 07:25:45 +0100

    20.287 - Adding the Auth Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit
 import {DropdownDirective} from './shared/directives/dropdown.directive';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+import { AuthComponent } from './auth/auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
