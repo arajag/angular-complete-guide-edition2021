aaada59a87d9d4593dd90410c564d97ed7c2ea16
acrajag <andre.raja@gmail.com>
2021-09-24 21:14:31 +0100

    22.326 - Understanding Shared Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,27 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {DropdownDirective} from './shared/directives/dropdown.directive';
 import {AuthComponent} from './auth/auth.component';
-import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner/loading-spinner.component';
 import {AuthInterceptor} from './auth/auth.interceptor';
-import {AlertComponent} from './shared/alert/alert.component';
-import {PlaceholderDirective} from './shared/placeholder/placeholder.directive';
 import {RecipesModule} from './recipes/recipes.module';
 import {ShoppingListModule} from './shopping-list/shopping-list-modules/shopping-list/shopping-list.module';
+import {FormsModule} from '@angular/forms';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
     AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
-    ShoppingListModule
+    ShoppingListModule,
+    SharedModule
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
