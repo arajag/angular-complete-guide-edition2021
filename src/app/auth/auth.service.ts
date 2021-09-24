08e94dde37bcef49194540a423d0a373b83aff2c
acrajag <andre.raja@gmail.com>
2021-09-24 07:46:00 +0100

    20.303 - Adding Auto-Login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -51,6 +51,16 @@ export class AuthService {
       }));
   }
 
+  autoLogin() {
+    const userData: UserData = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) return;
+    const loadedUser = new User(userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate));
+    if (loadedUser.token) this.user.next(loadedUser);
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -60,6 +70,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
@@ -80,5 +91,11 @@ export class AuthService {
     }
     return throwError(errorMessage);
   }
+}
 
+type UserData = {
+  email: string,
+  id: string,
+  _token: string,
+  _tokenExpirationDate: string
 }
