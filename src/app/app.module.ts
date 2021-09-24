ca8a3e3415c0afb15fb79843e147f1ccf727ad46
acrajag <andre.raja@gmail.com>
2021-09-24 22:05:49 +0100

    22.328 - Adding an Auth Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,25 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {AuthComponent} from './auth/auth.component';
 import {AuthInterceptor} from './auth/auth.interceptor';
 import {RecipesModule} from './recipes/recipes.module';
 import {ShoppingListModule} from './shopping-list/shopping-list-modules/shopping-list/shopping-list.module';
-import {FormsModule} from '@angular/forms';
 import {SharedModule} from './shared/shared.module';
+import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    AuthModule
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
