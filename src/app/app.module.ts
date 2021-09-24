54e89600fd880221606835775adae1a63bd65603
acrajag <andre.raja@gmail.com>
2021-09-24 22:30:45 +0100

    22.330 - Implementing Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {AuthInterceptor} from './auth/auth.interceptor';
-import {RecipesModule} from './recipes/recipes.module';
 import {ShoppingListModule} from './shopping-list/shopping-list-modules/shopping-list/shopping-list.module';
 import {SharedModule} from './shared/shared.module';
 import {AuthModule} from './auth/auth.module';
@@ -20,7 +19,6 @@ import {AuthModule} from './auth/auth.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     AuthModule
